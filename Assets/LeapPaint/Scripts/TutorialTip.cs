47c713726ce81f0c8761636abcc953712e68fe1c
jselstad <jselstad@leapmotion.com>
2016-09-14 12:56:54 -0700

    Add initial tutorial text

---
+++ Assets/LeapPaint/Scripts/TutorialTip.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialTip : MonoBehaviour {
+  public EmergeableBehaviour UIElement;
+  public TextMesh text;
+
+  private bool hasShownOnce = false;
+
+	void Start () {
+    text.gameObject.SetActive(false);
+    UIElement.OnFinishedEmerging += DoOnFinishedEmerging;
+    UIElement.OnBegunVanishing += DoOnBegunVanishing;
+	}
+
+  void DoOnFinishedEmerging() {
+    if (!hasShownOnce) {
+      text.gameObject.SetActive(true);
+      hasShownOnce = true;
+    }
+  }
+
+  void DoOnBegunVanishing() {
+    text.gameObject.SetActive(false);
+  }
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
