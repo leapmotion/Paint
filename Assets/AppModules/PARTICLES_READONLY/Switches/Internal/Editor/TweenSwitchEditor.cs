957043ca8bece3f484f33efead3616695b4f3a31
Nicholas Benson <nbenson@leapmotion.com>
2017-11-06 15:03:42 -0800

    Clear PARTICLES_READONLY in prep for particles appmodules migration

--- Assets/AppModules/PARTICLES_READONLY/Switches/Internal/Editor/TweenSwitchEditor.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEditor;
-using UnityEngine;
-
-namespace Leap.Unity.Animation {
-
-  [CustomEditor(typeof(TweenSwitch), editorForChildClasses: true)]
-  public class TweenSwitchEditor : SwitchEditorBase<TweenSwitch> { }
-
-}
