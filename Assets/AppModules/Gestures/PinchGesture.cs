d1da6d3eb8a1a11d87c6f9c02a9cef01d27541f1
Nicholas Benson <nbenson@leapmotion.com>
2017-12-01 16:27:02 -0800

    Add segment distance and hand velocity to pinch gesture

--- Assets/AppModules/Gestures/PinchGesture.cs
+++ Assets/AppModules/Gestures/PinchGesture.cs
@@ -12,17 +12,103 @@ namespace Leap.Unity.Gestures {
     private Pose _lastPinchPose = Pose.identity;
 
     public static float GetCustomPinchStrength(Hand h) {
-      var i = h.GetIndex().TipPosition.ToVector3();
-      var t = h.GetThumb().TipPosition.ToVector3();
+      var indexDistal = h.GetIndex().bones[3].PrevJoint.ToVector3();
+      var indexTip = h.GetIndex().TipPosition.ToVector3();
+      var thumbDistal = h.GetThumb().bones[3].PrevJoint.ToVector3();
+      var thumbTip = h.GetThumb().TipPosition.ToVector3();
 
-      var ti = i - t;
+      //var pinchDistance = (indexTip - thumbTip).magnitude;
+      var pinchDistance = Segment2SegmentDisplacement(indexDistal, indexTip,
+                                                      thumbDistal, thumbTip).magnitude;
 
       if (Input.GetKeyDown(KeyCode.C)) {
-        Debug.Log(ti.magnitude);
+        Debug.Log(pinchDistance);
       }
 
-      return ti.magnitude.MapUnclamped(0.0168f, 0.08f, 1f, 0f);
+      return pinchDistance.MapUnclamped(0.0168f, 0.08f, 1f, 0f);
     }
+
+    #region Segment-to-Segment Displacement (John S)
+
+    public static Vector3 Segment2SegmentDisplacement(Vector3 a1, Vector3 a2,
+                                                      Vector3 b1, Vector3 b2) {
+      float outTimeToA2 = 0f, outTimeToB2 = 0f;
+      return Segment2SegmentDisplacement(a1, a2, b1, b2, out outTimeToA2, out outTimeToB2);
+    }
+
+    public static Vector3 Segment2SegmentDisplacement(Vector3 a1, Vector3 a2,
+                                                      Vector3 b1, Vector3 b2,
+                                                      out float timeToa2,
+                                                      out float timeTob2) {
+      Vector3 u = a2 - a1; //from a1 to a2
+      Vector3 v = b2 - b1; //from b1 to b2
+      Vector3 w = a1 - b1;
+      float a = Vector3.Dot(u, u);         // always >= 0
+      float b = Vector3.Dot(u, v);
+      float c = Vector3.Dot(v, v);         // always >= 0
+      float d = Vector3.Dot(u, w);
+      float e = Vector3.Dot(v, w);
+      float D = a * c - b * b;        // always >= 0
+      float sc, sN, sD = D;       // sc = sN / sD, default sD = D >= 0
+      float tc, tN, tD = D;       // tc = tN / tD, default tD = D >= 0
+
+      // compute the line parameters of the two closest points
+      if (D < Mathf.Epsilon) { // the lines are almost parallel
+        sN = 0.0f;         // force using point P0 on segment S1
+        sD = 1.0f;         // to prevent possible division by 0.0 later
+        tN = e;
+        tD = c;
+      }
+      else {                 // get the closest points on the infinite lines
+        sN = (b * e - c * d);
+        tN = (a * e - b * d);
+        if (sN < 0.0f) {        // sc < 0 => the s=0 edge is visible
+          sN = 0.0f;
+          tN = e;
+          tD = c;
+        }
+        else if (sN > sD) {  // sc > 1  => the s=1 edge is visible
+          sN = sD;
+          tN = e + b;
+          tD = c;
+        }
+      }
+
+      if (tN < 0.0) {            // tc < 0 => the t=0 edge is visible
+        tN = 0.0f;
+        // recompute sc for this edge
+        if (-d < 0.0)
+          sN = 0.0f;
+        else if (-d > a)
+          sN = sD;
+        else {
+          sN = -d;
+          sD = a;
+        }
+      }
+      else if (tN > tD) {      // tc > 1  => the t=1 edge is visible
+        tN = tD;
+        // recompute sc for this edge
+        if ((-d + b) < 0.0)
+          sN = 0;
+        else if ((-d + b) > a)
+          sN = sD;
+        else {
+          sN = (-d + b);
+          sD = a;
+        }
+      }
+      // finally do the division to get sc and tc
+      sc = (Mathf.Abs(sN) < Mathf.Epsilon ? 0.0f : sN / sD);
+      tc = (Mathf.Abs(tN) < Mathf.Epsilon ? 0.0f : tN / tD);
+
+      // get the difference of the two closest points
+      Vector3 dP = w + (sc * u) - (tc * v);  // =  S1(sc) - S2(tc)
+      timeToa2 = sc; timeTob2 = tc;
+      return dP;   // return the closest distance
+    }
+
+    #endregion
 
     #region OneHandedGesture
 
@@ -30,7 +116,9 @@ namespace Leap.Unity.Gestures {
     public bool _drawDebug = false;
     public bool _drawDebugPath = false;
 
-    private DeltaFloatBuffer pinchStrengthBuffer = new DeltaFloatBuffer(10);
+    private DeltaFloatBuffer pinchStrengthBuffer = new DeltaFloatBuffer(5);
+
+    private DeltaBuffer handPositionBuffer = new DeltaBuffer(5);
 
     private const int MIN_REACTIVATE_TIME = 5;
     private int minReactivateTimer = 0;
@@ -49,6 +137,7 @@ namespace Leap.Unity.Gestures {
           
 
           pinchStrengthBuffer.Add(latestPinchStrength, Time.time);
+          handPositionBuffer.Add(hand.PalmPosition.ToVector3(), Time.time);
 
           if (pinchStrengthBuffer.IsFull) {
             var pinchStrengthVelocity = pinchStrengthBuffer.Delta();
@@ -63,8 +152,13 @@ namespace Leap.Unity.Gestures {
                 + Camera.main.transform.forward * 1f, Vector3.up, Color.red, 0.02f);
             }
 
-            if (pinchStrengthVelocity > 6f
-                && latestPinchStrength > 1.0f
+            var pinchActivateVelocity = 5f;
+            var handVelocity = handPositionBuffer.Delta();
+
+            pinchActivateVelocity = handVelocity.magnitude.Map(0f, 2f, 4f, 20f);
+
+            if (pinchStrengthVelocity > pinchActivateVelocity
+                && latestPinchStrength > 0.8f
                 && handWithinFOV) {
               shouldActivate = true;
               if (_drawDebug) {
