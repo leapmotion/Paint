8d1a66384666eccd0655fc51d6149266827f1e11
Nicholas Benson <nbenson@leapmotion.com>
2018-01-23 11:59:30 -0800

    Removed hacky need for director in EventTrack

--- Assets/LeapMotion/Modules/HierarchyRecording/Scripts/Events/EventPlayableMixerBehaviour.cs
+++ Assets/LeapMotion/Modules/HierarchyRecording/Scripts/Events/EventPlayableMixerBehaviour.cs
@@ -17,15 +17,13 @@ using UnityEngine.Timeline;
 namespace Leap.Unity.Recording {
 
   public class EventPlayableMixerBehaviour : PlayableBehaviour {
-
-    public PlayableDirector director;
+    
     public EventTrack eventTrack;
 
     private List<TimelineClip> _clips;
     private TimelineClipComparerer _clipComparer;
 
     private bool _firstFrameFired = false;
-    private double _lastTime = 0;
 
     #region PlayableBehaviour Events
 
@@ -33,7 +31,7 @@ namespace Leap.Unity.Recording {
       base.OnGraphStart(playable);
 
       //TEMP HACK:
-      director = UnityEngine.Object.FindObjectOfType<PlayableDirector>();
+      //director = UnityEngine.Object.FindObjectOfType<PlayableDirector>();
 
       if (_clips == null) {
         _clips = Pool<List<TimelineClip>>.Spawn();
@@ -43,7 +41,6 @@ namespace Leap.Unity.Recording {
       }
 
       _firstFrameFired = false;
-      _lastTime = 0;
     }
 
     public override void OnGraphStop(Playable playable) {
@@ -59,7 +56,6 @@ namespace Leap.Unity.Recording {
       }
 
       _firstFrameFired = false;
-      _lastTime = 0;
     }
 
     public override void PrepareFrame(Playable playable, FrameData info) {
@@ -71,28 +67,28 @@ namespace Leap.Unity.Recording {
     public override void ProcessFrame(Playable playable, FrameData info, object playerData) {
       base.ProcessFrame(playable, info, playerData);
 
+      // Get time of the root playable in the playable graph, which is the timeline itself.
+      var curTime = playable.GetGraph().GetRootPlayable(0).GetTime();
+      var prevTime = playable.GetGraph().GetRootPlayable(0).GetPreviousTime();
+
       // If there's an event at the beginning of the track, and we're currently at the
       // beginning of the track, we should fire it now.
-      checkFireTimeZeroEvent();
+      checkFireTimeZeroEvent(curTime);
 
       if (!_firstFrameFired) {
         _firstFrameFired = true;
       }
       else {
-        double curTime = director.time;
-        
-        if (_lastTime <= curTime) {
+        if (prevTime <= curTime) {
           // Sweep forward from prevTime to curTime if moving forward.
-          sweepFireEvents(_lastTime, curTime);
+          sweepFireEvents(prevTime, curTime);
         }
         else {
           // When scrubbing backwards, sweep from the beginning of the track, potentially
           // firing events based on the eventScrubType.
-          sweepFireFromBeginning(_lastTime, curTime);
+          sweepFireFromBeginning(prevTime, curTime);
         }
       }
-
-      _lastTime = director.time;
     }
 
     #endregion
@@ -136,8 +132,8 @@ namespace Leap.Unity.Recording {
     /// requires a previous frame time and a current frame time, which is unavailable
     /// at the beginning of a timeline.
     /// </summary>
-    private void checkFireTimeZeroEvent() {
-      if (director.time == 0 && Application.isPlaying) {
+    private void checkFireTimeZeroEvent(double curLocalTime) {
+      if (curLocalTime == 0f && Application.isPlaying) {
         if (_clips.Count > 0) {
           var firstClip = _clips[0];
           if (firstClip.start == 0) {
