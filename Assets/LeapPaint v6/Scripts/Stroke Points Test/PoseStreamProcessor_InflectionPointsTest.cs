2956176265994ee0ca9745238fc2172d264d8d7b
Nicholas Benson <nbenson@leapmotion.com>
2018-01-24 18:18:49 -0800

    more stream stuff

--- Assets/LeapPaint v6/Scripts/Stroke Points Test/PoseStreamProcessor_InflectionPointsTest.cs
+++ Assets/LeapPaint v6/Scripts/Stroke Points Test/PoseStreamProcessor_InflectionPointsTest.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 namespace Leap.Unity {
 
   public class PoseStreamProcessor_InflectionPointsTest : MonoBehaviour,
-                                                          IStreamReceiver<Pose> {
+                                                          IStreamReceiver<Pose>,
+                                                          IStream<Pose> {
 
     public float maxSkipDistance = 0.10f;
     public float maxSkipAngle = 5f;
@@ -14,22 +15,35 @@ namespace Leap.Unity {
     private Pose? _lastOutputPose = null;
     private List<Pose> _skippedPoses = new List<Pose>();
 
+    // Stream<Pose> events
+    public event Action OnOpen = () => { };
+    public event Action<Pose> OnSend = (pose) => { };
+    public event Action OnClose = () => { };
+
     public void Open() {
       _lastOutputPose = null;
       _skippedPoses.Clear();
+
+      OnOpen();
     }
 
     public void Receive(Pose pose) {
-      
+      Pose? outputPose = null;
+
       if (!_lastOutputPose.HasValue) {
 
       }
-
+      
+      if (outputPose.HasValue) {
+        OnSend(outputPose.Value);
+      }
     }
 
     public void Close() {
       _lastOutputPose = null;
       _skippedPoses.Clear();
+
+      OnClose();
     }
 
   }
