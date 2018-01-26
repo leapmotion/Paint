f6c742ac448942fda704121edf6474daa36eb6bc
Alex Marcolina <amarcolina@gmail.com>
2018-01-25 16:31:35 -0800

    adde dthe thick line recorder

---
+++ Assets/archives/LeapPaint (v3 archive)/zzOld_LeapPaint/Scripts/Tutorial/ThickLineRendererRecorder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Recording;
+using Leap.Unity.LeapPaint_v3;
+
+public class ThickLineRendererRecorder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
