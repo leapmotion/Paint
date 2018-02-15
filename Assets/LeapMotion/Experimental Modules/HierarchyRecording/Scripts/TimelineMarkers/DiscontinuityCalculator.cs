f7118f9d75e97fa3f6e9820d8ce4c5adbec34044
Alex Marcolina <amarcolina@gmail.com>
2018-02-15 15:30:04 -0800

    added better head smoothing

---
+++ Assets/LeapMotion/Experimental Modules/HierarchyRecording/Scripts/TimelineMarkers/DiscontinuityCalculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Leap.Unity.Recording {
+
+  [RequireComponent(typeof(PlayableDirector))]
+  public class DiscontinuityCalculator : MonoBehaviour {
+    public Action<bool> OnUpdate;
+
+    public List<string> tracksToWatch = new List<string>();
+
+    private PlayableDirector _director;
+    private List<TimelineClip> _clipsToWatch = new List<TimelineClip>();
+
+    private PlayableAsset _prevAsset;
+    private double _prevTime;
+
+    private void Awake() {
+      _director = GetComponent<PlayableDirector>();
+    }
+
+    private void LateUpdate() {
+      bool isDiscontinuity = false;
+
+      //If we switched to a different asset
+      if (_director.playableAsset != _prevAsset) {
+        isDiscontinuity = true;
+        recalculateClips();
+      }
+
+      //If we moved back in time
+      if (_director.time < _prevTime) {
+        isDiscontinuity = true;
+      }
+
+      //Or if we moved forward in time more than we should have
+      if (_director.time - _prevTime > Time.deltaTime * 3) {
+        isDiscontinuity = true;
+      }
+
+      //Or if we entered or left any clips that we are watching
+      foreach (var clip in _clipsToWatch) {
+        if (didEnterOrLeaveClip(clip)) {
+          isDiscontinuity = true;
+          break;
+        }
+      }
+
+      if (OnUpdate != null) {
+        OnUpdate(isDiscontinuity);
+      }
+
+      _prevTime = _director.time;
+      _prevAsset = _director.playableAsset;
+    }
+
+    private void recalculateClips() {
+      _clipsToWatch.Clear();
+
+      var timeline = _director.playableAsset as TimelineAsset;
+      if (timeline == null) {
+        return;
+      }
+
+      for (int i = 0; i < timeline.outputTrackCount; i++) {
+        var track = timeline.GetOutputTrack(i);
+        Debug.Log(track.name);
+        if (tracksToWatch.Contains(track.name)) {
+          _clipsToWatch.AddRange(track.GetClips());
+        }
+      }
+    }
+
+    private bool didEnterOrLeaveClip(TimelineClip clip) {
+      bool wasInside = _prevTime > clip.start && _prevTime < clip.end;
+      bool isInside = _director.time > clip.start && _director.time < clip.end;
+      return wasInside != isInside;
+    }
+  }
+}
--- Assets/archives/LeapPaint (v3 archive)/zzOld_LeapPaint/Tutorial/HeadSmoother.cs
+++ Assets/archives/LeapPaint (v3 archive)/zzOld_LeapPaint/Tutorial/HeadSmoother.cs
@@ -2,56 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Leap.Unity;
+using Leap.Unity.Recording;
 
 [ExecuteInEditMode]
 public class HeadSmoother : MonoBehaviour {
 
+  public DiscontinuityCalculator discontinuity;
   public Transform src;
-  public float positionThresh = 0.01f;
-  public float rotationThresh = 10;
+
+  [Header("Smoothing Settings")]
+  public bool performSmoothing = true;
   public float returnStrengthMult = 1;
   public AnimationCurve positionReturnStrength;
   public AnimationCurve rotationReturnStrength;
-  public int warmUpFrames = 5;
-
-  private Vector3 _position0, _position1, _position2;
-  private Quaternion _rotation0, _rotation1, _rotation2;
 
   private Vector3 _offsetPosition;
   private Quaternion _offsetRotation;
 
-  private void Update() {
+  private void OnEnable() {
+    discontinuity.OnUpdate += onUpdateDiscontiuity;
+  }
+
+  private void OnDisable() {
+    discontinuity.OnUpdate -= onUpdateDiscontiuity;
+  }
+
 #if UNITY_EDITOR
+  private void Update() {
     if (!Application.isPlaying) {
       transform.position = src.position;
       transform.rotation = src.rotation;
       return;
     }
+  }
 #endif
 
-    if (Time.frameCount < warmUpFrames) {
-      transform.position = src.position;
-      transform.rotation = src.rotation;
+  private void onUpdateDiscontiuity(bool isDiscontiuity) {
+    if (isDiscontiuity) {
+      _offsetPosition = src.position.To(transform.position);
+      _offsetRotation = src.rotation.To(transform.rotation);
     }
 
-    _position2 = _position1;
-    _position1 = _position0;
-    _position0 = src.position;
-
-    _rotation2 = _rotation1;
-    _rotation1 = _rotation0;
-    _rotation0 = src.rotation;
-
-    float deltaPosition21 = Vector3.Distance(_position2, _position1);
-    float deltaPosition10 = Vector3.Distance(_position1, _position0);
-
-    float deltaRotation21 = Quaternion.Angle(_rotation2, _rotation1);
-    float deltaRotation10 = Quaternion.Angle(_rotation1, _rotation0);
-
-    if (Mathf.Abs(deltaPosition10 - deltaPosition21) > positionThresh ||
-        Mathf.Abs(deltaRotation10 - deltaRotation21) > rotationThresh) {
-      _offsetPosition = _position0.To(transform.position);
-      _offsetRotation = _rotation0.To(transform.rotation);
+    if (!performSmoothing) {
+      _offsetPosition = Vector3.zero;
+      _offsetRotation = Quaternion.identity;
     }
 
     transform.position = src.position.Then(_offsetPosition);
@@ -63,5 +57,4 @@ public class HeadSmoother : MonoBehaviour {
     _offsetPosition = Vector3.Lerp(_offsetPosition, Vector3.zero, positionReturn);
     _offsetRotation = Quaternion.Lerp(_offsetRotation, Quaternion.identity, rotationReturn);
   }
-
 }
