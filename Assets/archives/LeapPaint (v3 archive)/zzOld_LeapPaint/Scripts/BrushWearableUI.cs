0a191043cda2999492ab246282d72050ecb6d16e
Nick Benson <nickjbenson@gmail.com>
2018-02-21 16:43:56 -0800

    Brush menu VanishNow on return to anchor

--- Assets/archives/LeapPaint (v3 archive)/zzOld_LeapPaint/Scripts/BrushWearableUI.cs
+++ Assets/archives/LeapPaint (v3 archive)/zzOld_LeapPaint/Scripts/BrushWearableUI.cs
@@ -84,9 +84,9 @@ namespace Leap.Unity.LeapPaint_v3 {
     protected override void DoOnReturnedToAnchor() {
       base.DoOnReturnedToAnchor();
       
-      _brushControlsEmergeable.TryVanish(IsWorkstation);
+      _brushControlsEmergeable.TryVanishNow(IsWorkstation);
       _brushControlsEmerged = false;
-      _brushWorkstationEmergeable.TryVanish(IsWorkstation);
+      _brushWorkstationEmergeable.TryVanishNow(IsWorkstation);
 
       _brushControlsMoveable.MoveToA();
     }
