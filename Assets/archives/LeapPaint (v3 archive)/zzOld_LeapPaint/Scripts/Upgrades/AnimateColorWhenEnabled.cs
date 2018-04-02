5723eb59b6446d7a89b809aa9648ddfb70f5446a
Nicholas Benson <nbenson@leapmotion.com>
2018-04-02 13:54:31 -0700

    Add color-oscillation to indicate swatch "receive" color mode

---
+++ Assets/archives/LeapPaint (v3 archive)/zzOld_LeapPaint/Scripts/Upgrades/AnimateColorWhenEnabled.cs
@@ -0,0 +1,62 @@
+﻿using Leap.Unity.Attributes;
+using UnityEngine;
+
+namespace Leap.Unity.LeapPaint {
+
+  public class AnimateColorWhenEnabled : MonoBehaviour {
+
+    [Header("Color States")]
+
+    public Color colorWhenEnabled0 = Color.white;
+    public Color colorWhenEnabled1 = Color.yellow;
+    public AnimationCurve animCurve = DefaultCurve.SigmoidUpDown;
+    [MinValue(0.01f)]
+    public float animPeriod = 2.0f;
+    private float _animT = 0f;
+    public Color colorWhenDisabled = Color.gray;
+
+    [Header("Renderer Reference")]
+
+    public Renderer rendererToDrive;
+    public struct RendererMaterialInstancePair {
+      public Renderer renderer;
+      public Material matInstance;
+    }
+    public RendererMaterialInstancePair _backingRendererMatPair
+      = new RendererMaterialInstancePair();
+    public Material materialInstance {
+      get {
+        // Edit-time color not supported.
+        if (!Application.isPlaying) return null;
+
+        // Cache material instance reference, but update it if the renderer changes.
+        if (rendererToDrive == null) { return null; }
+        else {
+          var pair = _backingRendererMatPair;
+          if (pair.renderer == null || pair.renderer != rendererToDrive) {
+            pair.renderer = rendererToDrive;
+            pair.matInstance = rendererToDrive.material; // creates material instance.
+          }
+          return pair.matInstance;
+        }
+      }
+    }
+
+    private void OnEnable() {
+      materialInstance.color = colorWhenEnabled0;
+      _animT = 0f;
+    }
+    private void Update() {
+      _animT += Time.deltaTime;
+      _animT %= animPeriod;
+
+      materialInstance.color
+        = Color.Lerp(colorWhenEnabled0, colorWhenEnabled1, animCurve.Evaluate(_animT));
+    }
+    private void OnDisable() {
+      materialInstance.color = colorWhenDisabled;
+    }
+
+  }
+
+}
---
+++ Assets/archives/LeapPaint (v3 archive)/zzOld_LeapPaint/Scripts/Upgrades/ChangeColorWhenEnabled.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Leap.Unity.LeapPaint {
+
+  public class ChangeColorWhenEnabled : MonoBehaviour {
+
+    [Header("Color States")]
+
+    public Color colorWhenEnabled = Color.white;
+    public Color colorWhenDisabled = Color.gray;
+
+    [Header("Renderer Reference")]
+
+    public Renderer rendererToDrive;
+    public struct RendererMaterialInstancePair {
+      public Renderer renderer;
+      public Material matInstance;
+    }
+    public RendererMaterialInstancePair _backingRendererMatPair
+      = new RendererMaterialInstancePair();
+    public Material materialInstance {
+      get {
+        // Edit-time color not supported.
+        if (!Application.isPlaying) return null;
+
+        // Cache material instance reference, but update it if the renderer changes.
+        if (rendererToDrive == null) { return null; }
+        else {
+          var pair = _backingRendererMatPair;
+          if (pair.renderer == null || pair.renderer != rendererToDrive) {
+            pair.renderer = rendererToDrive;
+            pair.matInstance = rendererToDrive.material; // creates material instance.
+          }
+          return pair.matInstance;
+        }
+      }
+    }
+
+    private void OnEnable() {
+      materialInstance.color = colorWhenEnabled;
+    }
+    private void OnDisable() {
+      materialInstance.color = colorWhenDisabled;
+    }
+
+  }
+
+}
