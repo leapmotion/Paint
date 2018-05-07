51f94be7ee444efa74aee489d7c229334b001da6
Nicholas Benson <nbenson@leapmotion.com>
2018-05-07 15:52:50 -0700

    Add secondary camera, preview texture with "C"

---
+++ Assets/LeapPaint (v3)/Scripts/Upgrades/ActivateDisplays.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ActivateDisplays : MonoBehaviour {
+
+  void Start() {
+    Debug.Log("Displays connected: " + Display.displays.Length);
+
+    // Display.displays[0] is the primary, default display and is always ON.
+    // Check if additional displays are available and activate each.
+    if (Display.displays.Length > 1) {
+      Display.displays[1].Activate();
+    }
+  }
+
+}
